801380e19259313689cb75f3d0054ebc64a21eb1
zhuwanhong <40489974+zhuwanhong@users.noreply.github.com>
2023-02-05 21:44:33 +0900

    Create exception.cpp

---
+++ exceptions/exception.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	cout << "Do you want Exception?" << endl;
+	string input;
+	cin >> input;
+	try {
+		if (input == "int") {
+			throw 1;
+		}
+		if (input == "string") {
+			throw "string";
+		}
+		throw 4.3;
+	}
+	catch (int i) {
+		cout << "Hello int Exception" << endl;
+		return 1;
+	}
+	catch (string s) {
+		cout << "Hello string Exception" << endl;
+		return 1;
+	}
+	catch (...) {
+		cout << "Hello Other Exception" << endl;
+		return 1;
+	}
+	cout << "Hello No Exception" << endl;
+	return 0;
+}
